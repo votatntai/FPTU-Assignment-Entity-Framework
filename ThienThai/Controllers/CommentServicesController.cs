8cf81702b92ec3dceb694b245af8036290885628
Vo Tan Tai <votantai4899@gmail.com>
2020-07-12 23:49:44 +0700

    Add API

---
+++ ThienThai/Controllers/CommentServicesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Data.Entities;
+
+namespace ThienThai.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentServicesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentServicesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CommentServices
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comments>>> GetComments()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+
+        // GET: api/CommentServices/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comments>> GetComments(int id)
+        {
+            var comments = await _context.Comments.FindAsync(id);
+
+            if (comments == null)
+            {
+                return NotFound();
+            }
+
+            return comments;
+        }
+
+        // PUT: api/CommentServices/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComments(int id, Comments comments)
+        {
+            if (id != comments.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(comments).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommentsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CommentServices
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Comments>> PostComments(Comments comments)
+        {
+            _context.Comments.Add(comments);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetComments", new { id = comments.ID }, comments);
+        }
+
+        // DELETE: api/CommentServices/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Comments>> DeleteComments(int id)
+        {
+            var comments = await _context.Comments.FindAsync(id);
+            if (comments == null)
+            {
+                return NotFound();
+            }
+
+            _context.Comments.Remove(comments);
+            await _context.SaveChangesAsync();
+
+            return comments;
+        }
+
+        private bool CommentsExists(int id)
+        {
+            return _context.Comments.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ ThienThai/Controllers/IdolServicesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Data.Entities;
+
+namespace ThienThai.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdolServicesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IdolServicesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/IdolServices
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Idols>>> GetIdols()
+        {
+            return await _context.Idols.ToListAsync();
+        }
+
+        // GET: api/IdolServices/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Idols>> GetIdols(int id)
+        {
+            var idols = await _context.Idols.FindAsync(id);
+
+            if (idols == null)
+            {
+                return NotFound();
+            }
+
+            return idols;
+        }
+
+        // PUT: api/IdolServices/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutIdols(int id, Idols idols)
+        {
+            if (id != idols.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(idols).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IdolsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/IdolServices
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Idols>> PostIdols(Idols idols)
+        {
+            _context.Idols.Add(idols);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetIdols", new { id = idols.ID }, idols);
+        }
+
+        // DELETE: api/IdolServices/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Idols>> DeleteIdols(int id)
+        {
+            var idols = await _context.Idols.FindAsync(id);
+            if (idols == null)
+            {
+                return NotFound();
+            }
+
+            _context.Idols.Remove(idols);
+            await _context.SaveChangesAsync();
+
+            return idols;
+        }
+
+        private bool IdolsExists(int id)
+        {
+            return _context.Idols.Any(e => e.ID == id);
+        }
+    }
+}
